8ca18c77e7f8957bca319b95525a5251ff41afe2
FranckNdame <franck.mpouli@yahoo.com>
2019-12-06 15:29:25 +0000

    update replay buffer

--- src/utils/memory.py
+++ src/utils/memory.py
@@ -1,28 +1,45 @@
-from collections import deque
+from collections import deque, namedtuple
 import numpy as np
-
 import random
-class Memory(object):
-    def __init__(self, buffer_size=1_000_000):
-        self.buffer = deque(maxlen=buffer_size)
 
-    def save(self, experience):
+class Memory(object):
+    """Replay memory buffer to store experience tuples."""
+    def __init__(self, action_size, buffer_size=1_000_000, batch_size=64, seed):
+        """Initialize a ReplayBuffer object.
+        
+        Params
+        ======
+            action_size (int): size of each action
+            buffer_size (int): size of memory buffer
+            batch_size (int): size of each training minibatch
+            seed (int): random seed
+        """
+        self.action_size = action_size
+        self.buffer = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.seed = seed
+        
+        self.experience = namedtuple("Experience", field_names=["states", "actions", "reward", "next_state", "done"])
+
+    def save(self, state, action, reward, next_state, done):
+        """Save new experience to buffer"""
+        experience = self.experience(state, action, reward, next_state, done)
         self.buffer.append(experience)
     
-    def sample(self, batch_size):
-        sample_size = min(len(self.buffer), batch_size)
-        samples = random.choices(self.buffer, k=sample_size)
-        return map(list, zip(*samples))
-
-#    def sample(self, batch_size):
-#        if batch_size > len(self.buffer):
-#            return
-#        index = np.random.choice(
-#            np.arange(len(self.buffer)),
-#            size = batch_size,
-#            replace = False
-#        )
-#        return [self.buffer[i] for i in index]
-
-    def can_provide(self, batch_size):
-        return len(self.buffer) >= batch_size
+    def sample(self):
+        """Sample a batch of random experiences from memory"""
+        samples = random.sample(self.buffer, k=self.batch_size)
+        
+        states = torch.from_numpy(np.vstack([sample.state for sample in samples if sample is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([sample.action for sample in samples if sample is not None])).long().to(device)
+        rewards = torch.from_numpy(np.vstack([sample.reward for sample in samples if sample is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([sample.next_state for sample in samples if sample is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([sample.done for sample in samples if sample is not None]).astype(np.uint8)).float().to(device)
+          
+        return (states, actions, rewards, next_states, dones)
+        
+        
+    @property
+    def can_provide(self):
+        """Check if the buffer can provide samples"""
+        return len(self.buffer) >= self.batch_size
