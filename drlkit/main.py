e8fde619499b99976a287bce1f31c313cb0d8018
FranckNdame <franck.mpouli@yahoo.com>
2019-12-11 20:00:09 +0000

    add demo video

--- drlkit/main.py
+++ drlkit/main.py
@@ -5,19 +5,19 @@ from environments.wrapper import EnvironmentWrapper
 import os
 import models
 
-ENV_NAME = "CartPole-v1"
+ENV_NAME = "LunarLander-v2"
 env = EnvironmentWrapper(ENV_NAME, max_ts=5000)
-agent = TorchAgent(state_size=4, action_size=env.env.action_space.n, seed=0)
+agent = TorchAgent(state_size=8, action_size=env.env.action_space.n, seed=0)
 
 ## Train the agent
-env.fit(agent, n_episodes=2_000)
+# env.fit(agent, n_episodes=2_000)
 #
 ## See the results
 #Plot.basic_plot(np.arange(len(env.scores)), env.scores, xlabel='Episode #', ylabel='Score')
 
 
-# env.load_prebuilt_model(agent, "./models/LunarLander-v2-4477.pth")
-env.play(num_episodes=20)
+env.load_prebuilt_model(agent, "./models/LunarLander-v2-4477.pth")
+env.play(num_episodes=15, trained=False)
 
 
 
